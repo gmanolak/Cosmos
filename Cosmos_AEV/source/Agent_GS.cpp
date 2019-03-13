836cc18d641778a88a34dbc30a2ce9d6f9a566b6
BeefBriskets <37361569+BeefBriskets@users.noreply.github.com>
2019-03-12 22:27:22 -1000

    Added a negative to the lattitude on the GS agent when printing

--- Cosmos_AEV/source/Agent_GS.cpp
+++ Cosmos_AEV/source/Agent_GS.cpp
@@ -13,7 +13,14 @@ int main(int argc, char** argv)
 	 int32_t iretn;
 	 Agent::messstruc mess;
 	 while(agent->running())
-	 {
+	 {A bank has two sites at which checks are processed. Site
+1 can process 10,000 checks per day, and site 2 can process
+6,000 checks per day. The bank processes three types of
+checks: vendor, salary, and personal. The processing cost
+per check depends on the site (see Table 11). Each day,
+5,000 checks of each type must be processed. Formulate a
+balanced transportation problem to minimize the daily cost
+of processing checks.
 		 iretn = agent->readring(mess, Agent::AgentMessage::BEAT, 5., (Agent::Where)1);
 		 if (iretn > 0 && !strcmp(mess.meta.beat.proc, "gps"))
 		 {
@@ -21,7 +28,7 @@ int main(int argc, char** argv)
 			 double gps_latitude = agent->cinfo->devspec.gps[0]->geods.lat;
 			 double gps_longitute = agent->cinfo->devspec.gps[0]->geods.lon;
 			  double gps_altitude = agent->cinfo->devspec.gps[0]->geods.h;
-			   cout << "GPS (lat, lon, h) = [" << gps_latitude << ", " <<  gps_longitute << ", " << gps_altitude << "]" << endl;
+			   cout << "GPS (lat, lon, h) = [" << gps_latitude << ", -" <<  gps_longitute << ", " << gps_altitude << "]" << endl;
 			   // GUI comman:wq
 			
 		 }
